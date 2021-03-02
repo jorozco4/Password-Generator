a5b9cc6120b778ff909c3897d7a6db058e9ec85b
Jesse Orozco <orozcojesse0@gmail.com>
2021-03-01 23:08:47 -0500

    just beginning on assignment

--- Develop/script.js
+++ Develop/script.js
@@ -3,12 +3,18 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  console.log("Hi I am Jesse");
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  console.log("passwordText");
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  var newPassword = "!@#$%%^^&";
+  newPassword = "jesse";
+  return newPassword;
+}
