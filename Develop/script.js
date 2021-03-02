5b2c6e0e67dcda6dee39d50c86036dd10e20fc18
Jesse Orozco <orozcojesse0@gmail.com>
2021-03-02 14:44:15 -0500

    adding few materials little by little

--- Develop/script.js
+++ Develop/script.js
@@ -14,7 +14,31 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-  var newPassword = "!@#$%%^^&";
-  newPassword = "jesse";
+  let passwordLegnth = prompt(
+    "choose a length of at least 8 characters and no more than 128 characters"
+  );
+
+  //If the password legnth is less than 8 or gretaer than 128 the form should cancel
+  if (passwordLegnth < 8 || passwordLegnth > 128) {
+    alert("Sorry it needs to follow the password requirements");
+  } else {
+    let upperCase = confirm("Is your password going to contain a uppercase?");
+    let lowerCase = confirm("Is your password going to contain a lowerase?");
+    let number = confirm("Is your password going to contain a number?");
+    let symbol = confirm("Is your password going to contain a symbol?");
+
+    if (upperCase || lowerCase || number || symbol) {
+      var randomstring = Math.random().toString().slice(-8);
+    }
+    // let randomstring = {
+    //   upperCase = ABCDEFGHIJKLMNOPQRSTUVWXYZ;
+    //   lowerCase = abcdefghijklmnopqrstuvwxyz;
+    //   number = 0123456789;
+    //   symbol = !@#$%^&*()_+;
+    // }
+  }
+
+  var newPassword = Math.random().toString().slice(-8);
   return newPassword;
 }
+/// Your choice of variables
